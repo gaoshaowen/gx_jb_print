8e65b61cea6cd2d3f2f7a4123be35b9905e7ede8
gaoshaowen <gsw@gaoshaowen.Topwisesz.com>
2019-08-19 15:22:47 +0800

    修改同步服务为 tms.topwisesz.com:8010

--- src/config_cli.js
+++ src/config_cli.js
@@ -2,7 +2,7 @@ module.exports ={
     //server: process.env.NODE_ENV=="production" ?'http://tms.topwisesz.com:8989': '/api'
     server: 'http://www.cnyyt.cn/wxback',
     
-    tongbuserver:'http://www.cnyyt.cn/tongbu/',
+    tongbuserver:'http://tms.topwisesz.com:8010/',
 
     virtualpath:'/zhifu/',
 }
